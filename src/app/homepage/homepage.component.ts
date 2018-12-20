3cf7ac9c284cf28aa2e4e2826e831542914b8d6a
Stefan Grasu <sgrasu@utexas.edu>
2018-12-19 20:28:04 -0600

    Revert " fix bubbles not filling home page, close #1"
    This reverts commit a27f38fd2330c348bd0ec74c00cd3e575c7aef5d.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,7 +12,7 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    particlesJS.load('home-container', './assets/particlesjs-config.json', function() {
+    particlesJS.load('home_container', './assets/particlesjs-config.json', function() {
       console.log('callback - particles.js config loaded');
     });
   }
